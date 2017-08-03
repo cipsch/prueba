f75af0e04e968ccece46a32e2563bb6ead246ada
Carlos Parada Schulthess <cparad2006@gmail.com>
2017-08-03 12:24:48 -0400

    primer commit

---
+++ src/utils/Calculadora.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Carlos Parada Schulthess
+ * Date: 03-08-17
+ * Time: 09:57
+ */
+class Calculadora
+{
+
+    public function sumar($x = 0, $y = 0) {
+        return $x + $y;
+    }
+
+    public function restar($x = 0, $y = 0) {
+        return $x - $y;
+    }
+}
